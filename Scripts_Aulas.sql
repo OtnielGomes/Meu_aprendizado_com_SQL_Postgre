b28f7de63fe3c3f5a898f179a13d4172c74376c4
Otniel Gomes <139892125+OtnielGomes@users.noreply.github.com>
2023-09-13 10:13:01 -0300

    2023-09-13

--- Scripts_Aulas.sql
+++ Scripts_Aulas.sql
@@ -62,3 +62,164 @@ VALUES(14, 'Jeferson', null, null, '1983-01-07', 'M', null, 'Brasileiro', null, 
 
 INSERT INTO cliente(idcliente, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
 VALUES(15, 'Jessica', null, null, null, 'F', 'Estudante', null, null, null, null, null, 'União da Vitória', 'PR');
+
+SELECT * FROM cliente;
+
+SELECT nome, data_nascimento
+FROM cliente;
+
+SELECT nome, data_nascimento 
+AS "Data de nascimento"
+FROM cliente;
+
+SELECT cpf, rg
+FROM cliente;
+
+SELECT 'CPF' || cpf|| 'RG' || rg 
+AS "CPF e RG" 
+FROM cliente;
+
+SELECT *
+FROM cliente
+LIMIT 3;
+
+SELECT nome, data_nascimento
+FROM cliente;
+
+SELECT nome, data_nascimento
+FROM cliente
+WHERE data_nascimento > '2000-01-01';
+
+SELECT nome
+FROM cliente
+WHERE nome 
+LIKE 'C%';
+
+SELECT nome
+FROM cliente
+WHERE nome
+LIKE '%c%';
+
+SELECT nome, data_nascimento
+FROM cliente
+WHERE data_nascimento
+BETWEEN '1990-01-01'
+AND '1998-01-01';
+
+SELECT nome, rg
+FROM cliente;
+
+SELECT nome, rg
+FROM cliente
+WHERE rg
+IS null;
+
+SELECT nome
+FROM cliente
+ORDER BY nome
+ASC;
+
+SELECT nome
+FROM cliente
+ORDER BY nome
+DESC;
+
+-- Exercícios
+
+--1. O nome, o gênero e a profissão de todos os clientes, ordenado pelo nome em ordem decrescente
+
+SELECT nome, genero, profissao
+FROM cliente
+ORDER BY nome
+DESC;
+
+--2. Os clientes que tenham a letra “R” no nome
+
+SELECT nome
+FROM cliente
+WHERE nome
+LIKE '%r%';
+
+--3. Os clientes que o nome inicia com a letra “C”
+
+SELECT nome
+FROM cliente
+WHERE nome
+LIKE 'C%';
+
+--4. Os clientes que o nome termina com a letra “A”
+
+SELECT nome
+FROM cliente
+WHERE nome
+LIKE '%a';
+
+--5. Os clientes que moram no bairro “Centro”
+
+SELECT nome, bairro
+FROM cliente
+WHERE bairro = 'Centro'
+OR bairro = 'Cto'
+OR bairro = 'Ctr.';
+
+--6. Os clientes que moram em complementos que iniciam com a letra “A”
+
+SELECT nome, complemento
+FROM cliente
+WHERE complemento
+LIKE 'A%';
+
+--7. Somente os clientes do sexo feminino
+
+SELECT nome, genero
+FROM cliente
+WHERE genero = 'F';
+
+--8. Os clientes que não informaram o CPF
+
+SELECT nome, cpf
+FROM cliente
+WHERE cpf
+IS null;
+
+--9. O nome e a profissão dos clientes, ordenado em ordem crescente pelo nome da profissão
+
+SELECT nome, profissao
+FROM cliente
+ORDER BY profissao
+ASC;
+
+--10. Os clientes de nacionalidade “Brasileira”
+
+SELECT nome, nacionalidade
+FROM cliente
+WHERE nacionalidade 
+LIKE 'Brasil%';
+
+--11. Os clientes que informaram o número da residência
+
+SELECT nome, numero
+FROM cliente
+WHERE numero
+IS not null;
+
+--12. Os clientes que moram em Santa Catarina
+
+SELECT nome, uf
+FROM cliente
+WHERE uf 
+LIKE 'SC';
+
+--13. Os clientes que nasceram entre 01/01/2000 e 01/01/2002
+
+SELECT nome, data_nascimento
+FROM cliente
+WHERE data_nascimento
+BETWEEN '2000-01-01'
+AND '2002-01-01';
+
+--14. O nome do cliente e o logradouro, número, complemento, bairro, município e UF concatenado de todos os clientes
+
+SELECT 'Nome: ' || nome || '- Logradouro: ' || logradouro || '- Número: ' || numero || '- Complemento: ' || complemento || '- Bairro: ' || bairro || '- Município: ' || municipio || '- UF: '|| uf
+AS "Todos os clientes"
+FROM cliente;
